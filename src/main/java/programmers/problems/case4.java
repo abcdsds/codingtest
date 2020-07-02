1b74438220bb0fa4417dc5ba2baeb975f88ac5c4
abcdefg <choiyurim2@naver.com>
2020-07-02 19:10:47 +0900

    리팩토링

--- src/main/java/programmers/problems/case4.java
+++ src/main/java/programmers/problems/case4.java
@@ -9,64 +9,39 @@ public class case4 {
 	public static int solution(int bridge_length, int weight, int[] truck_weights) {
 
 		int answer = 0;
-
+		int leftCount = weight;
 		Stack<Integer> wait = new Stack<Integer>();
+		Queue<Integer> moving = new LinkedList<Integer>();
+		Queue<Integer> count = new LinkedList<Integer>();
 		
 		for (int i=truck_weights.length-1; i>=0; i--) {
 			wait.push(truck_weights[i]);
 		}
 
-		Queue<Integer> moving = new LinkedList<Integer>();
-
-		Stack<Integer> destination = new Stack<Integer>();
-
-		Queue<Integer> count = new LinkedList<Integer>();
-
-		Integer pop = wait.pop();
-		moving.offer(pop);
-		count.offer(1);
-		int leftCount = weight - pop;
-		
 		while (true) {
 
 			answer++;
 			
-			if (count.peek().intValue() > bridge_length) {
-				System.out.println("push " + moving.peek());
-				System.out.println("push current count " + answer);
+			if (count.size() > 0 && count.peek().intValue() > bridge_length) {
 				Integer poll = moving.poll();
-				destination.push(count.poll());
+				count.poll();
 				leftCount += poll.intValue();
 				
 				if (wait.isEmpty() && moving.isEmpty()) {
 					break;
 				}
-				//break;
 			}
-
+			
 			if (!wait.isEmpty()) { 
-				
-				if (!moving.isEmpty()) {
-					
-					if (leftCount >= wait.peek().intValue()) {
-						System.out.println("leftCount " + leftCount);
-						System.out.println("current wait" + wait.peek());
-						System.out.println( "count  " + answer);
-						pop = wait.pop();
+				if ((!moving.isEmpty() && leftCount >= wait.peek().intValue()) || moving.isEmpty()) {
+						Integer pop = wait.pop();
 						moving.offer(pop);
 						count.offer(1);
 						leftCount -= pop.intValue();
-
-					}
-				} else {
-					pop = wait.pop();
-					moving.offer(pop);
-					count.offer(1);
-					leftCount -= pop.intValue();
-				}
-				
+				}				
 			}
-						
+					
+			
 			for (int i = 0; i < count.size(); i++) {
 				
 				Integer popCount = count.poll();
@@ -75,9 +50,6 @@ public class case4 {
 			}
 
 		}
-
-		System.out.println(destination);
-		System.out.println(answer);
 		
 		return answer;
 	}
@@ -98,7 +70,7 @@ public class case4 {
 		int weight = 2;
 		int[] truck_weights = { 1,1,1,1 };
 		
-		solution(bridge_length, weight, truck_weights);
+		System.out.println(solution(bridge_length, weight, truck_weights));
 
 	}
 }
