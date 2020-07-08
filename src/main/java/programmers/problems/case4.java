29aa6e825e8f88bb528a81b38ceef59ed0cb07d6
abcdefg <choiyurim2@naver.com>
2020-07-09 01:16:17 +0900

    스택 / 큐  / 프린터 문제 / 문제가 헷갈려서 오래걸림.

--- src/main/java/programmers/problems/case4.java
+++ src/main/java/programmers/problems/case4.java
@@ -2,9 +2,10 @@ package programmers.problems;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.stream.Collectors;
@@ -166,6 +167,135 @@ public class case4 {
         return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
 		
 	}
+	
+	public static int solution(int[] priorities, int location) {
+		int answer = 0;
+		//2 1 3 2 클수록 우선순위가 높다.
+		// 2,2,2,1,3,4
+		Queue<int[]> tempQueue = new LinkedList<int[]>();
+		Queue<int[]> resultQueue = new LinkedList<int[]>();
+		
+		for (int i=0; i<priorities.length; i++) {
+			tempQueue.offer(new int[]{i,priorities[i]});
+		}
+				
+		while (true) {
+			
+			if (resultQueue.size() == priorities.length) {
+				break;
+			}
+			Queue<int[]> queue = new LinkedList<int[]>();
+			queue.addAll(tempQueue);
+			
+			int[] temp = tempQueue.peek();
+			boolean check = true;
+			for (int i=0; i<tempQueue.size(); i++) {
+				int m = queue.peek()[0];
+				int l = queue.poll()[1];
+				if (temp[1] < l) {
+					System.out.println("=================");
+					//System.out.println(temp[0]);
+					System.out.println(temp[1]);
+					//System.out.println(m);
+					System.out.println(l);
+					System.out.println("=================");
+					for (int j=0; j<i; j++) {
+						int[] poll = tempQueue.poll();
+						tempQueue.offer(poll);
+					}
+					check = false;
+					break;
+				}
+			}
+			
+			if (check) {
+				int[] poll = tempQueue.poll();
+				System.out.println("Result Added = " +poll[1]);
+				resultQueue.offer(poll);
+				System.out.println(tempQueue.size());
+				System.out.println(resultQueue.size());
+			} else {
+				System.out.println("rest size " + queue.size());
+			}
+
+			System.out.println(check);
+		}
+		
+
+		//System.out.println("result QSize " + resultQueue.size());
+		
+		//System.out.println(tempQueue.peek()[0] + " : " + tempQueue.poll()[1]);
+		
+
+		int tempSize = tempQueue.size();
+		
+		for (int i=0; i<tempSize; i++) {
+			//System.out.println("temp " + tempQueue.peek()[0] + " : " + tempQueue.poll()[1]);
+			resultQueue.offer(tempQueue.poll());
+		}
+		
+		int resultSize = resultQueue.size();
+		
+		for (int i=0; i<resultSize; i++) {
+			System.out.println("result " + resultQueue.peek()[0] + " : " + resultQueue.poll()[1]);
+//			if (resultQueue.poll()[0] == location) {
+//				answer = (i+1);
+//			}
+		}
+		
+//		List<int[]> stack = new ArrayList<int[]>();
+//		
+//		for (int i=0; i<priorities.length; i++) {
+//			stack.add(new int[]{i,priorities[i]});
+//		}	
+//		
+//		
+//		Collections.sort(stack, new Comparator<int[]>() {
+//
+//			@Override
+//			public int compare(int[] o1, int[] o2) {
+//				// TODO Auto-generated method stub
+//				
+//				int i = o1[1];
+//				int j = o2[1];
+//				
+//				if (i == j) {
+//					int k = o1[0];
+//					int l = o2[0];
+//					
+//					return l - k;
+//				} 
+//				
+//				return j - i;
+//			}
+//			
+//		});
+//		
+//		for (int[] j : stack) {
+//			System.out.println(j[0] + " : " + j[1]);
+//		}
+//		
+//		int asInt2 = IntStream.range(0, stack.size()).filter(v -> stack.get(v)[0] == location).map(v -> v).min().getAsInt();
+//		System.out.println(asInt2+1);
+		
+//		for (int[] j : stack) {
+//			System.out.println(j[0] + " : " + j[1]);
+//		}
+		
+//		for (int i=0; i<priorities.length; i++) {
+//			int intValue = stack.peek().intValue();
+//			priorities[i] > 
+//		}
+		
+		
+		
+		
+		
+		
+		System.out.println("answer" + answer);
+        return answer;
+    }
+	
 	public static void main(String[] args) {
 
 //		int bridge_length = 2;
@@ -186,10 +316,10 @@ public class case4 {
 
 		// int[] heights = {3,9,9,3,5,7,2};
 
-		int[] progress = {40, 93, 30, 55, 60, 65};
-		int[] speeds = {60, 1, 30, 5 , 10, 7};
-
-		solution(progress, speeds);
+//		int[] progress = {40, 93, 30, 55, 60, 65};
+//		int[] speeds = {60, 1, 30, 5 , 10, 7};
+//
+//		solution(progress, speeds);
 		
 //		int[] solution2 = solution2(progress, speeds);
 //		
@@ -197,6 +327,23 @@ public class case4 {
 //		IntStream.range(0, solution2.length).forEach(v -> {
 //			System.out.println(solution2[v]);
 //		});
+		
+//		int[] priorities = {2,1,3,2};
+//		int location = 2;
+		
+//		int[] priorities = {1, 1, 9, 1, 1, 1};
+//		int location = 0;
+//		
+//		int[] priorities = {2,2,2,1,3,4};
+		//4,1,2,2,2,1,3
+		//
+//		int location = 3;
+
+		int[] priorities = {1 , 2 , 3 , 4};
+		int location = 2;
+		
+		
+		solution(priorities, location);
 
 	}
 }
