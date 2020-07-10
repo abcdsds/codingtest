75574611722055110e10d4799331f2c4c7999a78
abcdefg <choiyurim2@naver.com>
2020-07-11 00:37:31 +0900

    효율성 조건 통과 시간복잡도 O(n log n) or O(n)

--- src/main/java/programmers/problems/hash.java
+++ src/main/java/programmers/problems/hash.java
@@ -1,24 +1,31 @@
 package programmers.problems;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
+
 
 public class hash {
 
 	public static String solution(String[] participant, String[] completion) {
         String answer = "";
+        Map<String, Integer> collect = new HashMap();
         
-
-        
-        List<String> collect = IntStream.range(0, participant.length).mapToObj(n -> participant[n]).collect(Collectors.toList());
-        
-                
-        IntStream.range(0, completion.length).forEach(v -> {
-        	collect.remove(completion[v]);
+        IntStream.range(0, participant.length).forEach(v -> {
+        	collect.put(participant[v], collect.getOrDefault(participant[v], 0) + 1);
         });
         
+//        collect.forEach((k,v) -> {
+//        	System.out.println(k);
+//        	System.out.println(v);
+//        });
         
+        IntStream.range(0, completion.length).forEach(v -> {
+        	collect.replace(completion[v], collect.get(completion[v]) - 1);
+        });
+        
+        String key = collect.entrySet().stream().filter(v -> v.getValue() == 1).findFirst().get().getKey();
+        System.out.println(key);
         return answer;
     }
 	
